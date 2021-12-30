4564c71e6b497b9c0ca75505beb6122e53030df7
Nicolas <medela.nicolas@gmail.com>
2021-12-30 18:10:05 -0300

    Elimino comentarios

--- src/index.js
+++ src/index.js
@@ -22,14 +22,11 @@ function isMutant(dna) {
         console.error('Hubo un error -> ', error, ' \nhasta acá')
     }
 }
-
-
 // Convierte cada cadena de string en un array de strings 
 function decomposeDNA(dna) {
     var decomposedDNA = []
     dna.forEach(c => {
         try {
-
             decomposedDNA.push(c.split(''))
         } catch (error) {
             throw Error('No se recibió caracteres válidos')
@@ -37,21 +34,17 @@ function decomposeDNA(dna) {
     });
     return decomposedDNA
 }
-
 //Chequea si es NxN
 // function checkNxN(dna){
 //     getN(dna)
 // }
-
 //Obtiene el n de la matriz
 function getN(dna) {
     var n = dna.length
     var rows = checkNumberRows(dna, NMIN)
     var columns = checkNumberColumn(dna, NMIN)
     if (!(rows == columns)) throw Error('No coinciden el numero de filas con el numero de columnas ')
-
     return n
-
 }
 //chequea numeros de filas según el tamaño del array comprobando que cumpla 
 //con el N minimo que se pase por parametro
@@ -61,7 +54,6 @@ function checkNumberRows(dna, n) {
     if (!(rows >= n)) throw Error('No cumple cantidad minima de filas')
     return rows
 }
-
 //Debe recibir un Array donde cada elemento es Array [[],[]...]
 //Chequea que cada fila de la matriz contengan la misma cantidad de columnas
 //Devuleve el numero de columnas
@@ -70,20 +62,15 @@ function checkNumberColumn(dna, n) {
     var columns = dna[0].length // [ []<-- (ese tamaño), ..[]  ]
     //Compruebo si no cumple requisito de n 
     if (!(columns >= n)) throw Error('Una o más filas no cumple cantidad minima de columnas')
-
-
     //compruebo que las columnas tengan la misma cantidad de filas comparando con las 
-
     //alternativa (compara el primero consigo mismo)
     // if( dna.some(r=> r.length != columns)) throw Error(No coinciden numeros de columnas)
-
     for (let nrow = 1; nrow < dna.length; nrow++) {
         const nColumns = dna[nrow].length; // numero de columnas de la fila
         if (!(nColumns == columns)) throw Error('Algunas filas no contienen la misma cantidad de columnas')
     }
     return columns
 }
-
 //chequea que los strings se encuentren en la lista de requisitos
 function checkStrings(dna, strings) {
     dna.forEach(r => {
@@ -92,16 +79,12 @@ function checkStrings(dna, strings) {
         })
     })
 }
-
 function dnaMatch(dna, n, nOcurrences) {
     var matchs = 0
-
     //comienza secuencia chequando por filas
-
     matchs = searchOcurrencesRC(dna, n, nOcurrences, matchs, compareRow)
     // if (matchs > 1) return true
     console.log(matchs)
-
     // //comienza secuencia chequando por columnas
     matchs = searchOcurrencesRC(dna, n, nOcurrences, matchs, compareColumn)
     console.log(matchs)
@@ -109,22 +92,14 @@ function dnaMatch(dna, n, nOcurrences) {
     matchs = compareDiagonal(dna, n, nOcurrences, matchs, compareColumn)
     console.log(matchs)
     if (matchs > 1) return true
-
     return false
 }
-
 // Busqueda por fila y por columna
 function searchOcurrencesRC(dna, n, nOcurrences, matchs, compareBy) {
-    // var matchs=0
     for (let r = 0; r < n; r++) {
-        //Empiezo a comparar el primer elemento de la fila hasta que el index + 4(
-        //cantidad de caracteres que tiene que haber iguales) sea menor o igual a N
         var c = 0
-        // var row = dna[c];
         while (c + nOcurrences <= n) {
-            // var element = row[r];
             var ocurrences = 1
-            //comparo con los 3 siguientes
             var i = c + 1 //puntero que arranque en el siguiente
             //   dna[r][c] == dna[r][i]
             while (i < c + nOcurrences && compareBy(dna, r, c, i)) {
@@ -134,21 +109,12 @@ function searchOcurrencesRC(dna, n, nOcurrences, matchs, compareBy) {
             if (ocurrences == nOcurrences) {
                 console.log('Ocurrencias igual a ', ocurrences, ' en ', r, ' ', c)
                 matchs++
-                // c++  //descomentar si lo puedo superponer
             }
-            // else { //descomentar si lo puedo superponer
             c = i //salto al primero disinto
-            // } // descomentar si lo puedo superponer
-
-
-
             // if (matchs > 1) {
             //     console.log('Detecto ', matchs, ' matchs')
             //     return matchs
             // }
-
-
-
         }
     }
     return matchs
@@ -161,59 +127,41 @@ const compareColumn = (dna, column, row, nextRow) => {
     return dna[row][column] == dna[nextRow][column]
 }
 
-
 //buscar por diagonal
-//HACERRRRRRRRRRRRRRRRR, intentar todas las diagonales de una
-
 function compareDiagonal(dna, n, nOcurrences, matchs, compareBy) {
     var hasta=n-nOcurrences  
     //diagonal superior
     for (let r = 0; r <=hasta; r++) {
         // var c=r
         var j=0
-        // for (let f = r; f <n ; f++) {
             var f= r
             while(f+nOcurrences<=n){
             var ocurrences = 1
-            //comparo con los 3 siguientes
-            var i = f + 1 //puntero que arranque en el siguiente
+            var i = f + 1 
              var h=j+1   
-             console.log(j, ' ',f,' ->', dna[j][f], ' compara ', h, ' ', i, ' ',dna[h][i])
             while (i < f + nOcurrences &&  dna[j][f] == dna[h][i] ) {
                 ocurrences++
                 i++
                 h++
             }
             if (ocurrences == nOcurrences) {
-                console.log('Ocurrencias igual a ', ocurrences, ' en ', j, ' ', f)
                 matchs++
-                // c++  //descomentar si lo puedo superponer
             }
             f=i;
             j=h
-            // f++
-            // j++
         }
     }
     console.log('hay ', matchs)
-//  00 01 02 03 04
-//     11 12 13 14
-//        22 23 24
-//           33 34
-//              44
+
     //diagonal inferior
     for (let r = 0; r <hasta; r++) {
         var j=r+1
         var f=0
-        // for (let f = 0; f <n-r-1 ; f++) {
         while(f+nOcurrences <= n-r-1){    
             var ocurrences = 1
-            //comparo con los 3 siguientes
-            var i = f + 1 //puntero que arranque en el siguiente
+            var i = f + 1 
             var h=j+1 
-            console.log(j, ' ',f,' ->', dna[j][f], ' compara ', h, ' ', i, ' ',dna[h][i])
             while (f < r + nOcurrences &&  dna[j][f] == dna[h][i] ) {
-                // console.log('encontro ocurrencia con ',j,' ',f,' y ', h, ' ',i)
                 ocurrences++
                 i++
                 h++
@@ -221,30 +169,32 @@ function compareDiagonal(dna, n, nOcurrences, matchs, compareBy) {
             if (ocurrences == nOcurrences) {
                 console.log('Ocurrencias igual a ', ocurrences, ' en ', j, ' ', f)
                 matchs++
-                // c++  //descomentar si lo puedo superponer
             }
             f=i;
             j=h
-            // j++
         }
         
     }
     console.log('hay ', matchs)
-
-//  10 
-//  20 21 
-//  30 31 32 
-//  40 41 42 43 
- 
-// 2 * (n - nOcurrences) + 1 <- Cantidad de iteraciones
-// 4 - 4 0-> 1
-// 5 - 4  1-> 3
-// 6 - 4 2-> 5
-// 7 - 4  3-> 7
-// 8 4 4->9
-// 9->11
-// 10->13
     return matchs
-}
-
-isMutant(DNAEXAMPLE2)
+    }
+    
+    isMutant(DNAEXAMPLE2)
+    //  00 01 02 03 04
+    //     11 12 13 14
+    //        22 23 24
+    //           33 34
+    //              44
+    //  10 
+    //  20 21 
+    //  30 31 32 
+    //  40 41 42 43 
+     
+    // 2 * (n - nOcurrences) + 1 <- Cantidad de iteraciones
+    // 4 - 4 0-> 1
+    // 5 - 4  1-> 3
+    // 6 - 4 2-> 5
+    // 7 - 4  3-> 7
+    // 8 4 4->9
+    // 9->11
+    // 10->13
